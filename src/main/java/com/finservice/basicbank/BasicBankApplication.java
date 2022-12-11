3bc0acb5112a8527e6eefbbdea045515af3b6bbc
Reagan-C <onwuegbureagan@gmail.com>
2022-12-11 11:17:50 +0100

    Add EnableWebSecurity annotation

--- src/main/java/com/finservice/basicbank/BasicBankApplication.java
+++ src/main/java/com/finservice/basicbank/BasicBankApplication.java
@@ -2,8 +2,10 @@ package com.finservice.basicbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class BasicBankApplication {
 
 	public static void main(String[] args) {
